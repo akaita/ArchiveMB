52e62bd0577ebda6b98f4baaa65b9fcab3d03994
Mikel Pascual <mikel@akaita.com>
2015-05-27 19:48:13 +0200

    Database version = 1

--- app/src/main/java/com/akaita/fda/database/DatabaseHelper.java
+++ app/src/main/java/com/akaita/fda/database/DatabaseHelper.java
@@ -22,7 +22,7 @@ import java.sql.SQLException;
  */
 public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
     private static final String DATABASE_NAME = "database.db";
-    private static final int DATABASE_VERSION = 16;
+    private static final int DATABASE_VERSION = 1;
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
